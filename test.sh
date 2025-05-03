bb1325e0a7d3c9d70eb00e61cdce8f1403f52e7b
crowbait <31518769+crowbait@users.noreply.github.com>
2025-05-03 12:32:07 +0200

    simplify testing setup

--- test.sh
+++ test.sh
@@ -1,29 +1,42 @@
 #!/bin/bash
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-export RUN="$SCRIPT_DIR/xmlmath"
+DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+# declare common test input files
+TEST_DEFAULT="$DIR/test/test.xml"
+
+# declare all tests
+# format: [parameters for xmlmath]
+# the expected output is test/[name of test].xml
+
+declare -A tests=(
+  [target_attr]="attr add 1 -f $TEST_DEFAULT attr1"
+)
+
+
 
 ALL_OKAY="true"
 RUN_TESTS=0
 RUN_TESTS_SUCCESS=0
 RUN_TESTS_ERROR=0
+rm -f "$dir/testout_*"
 
-for script in "$SCRIPT_DIR/test"/*.sh; do
-
-  if [[ -f "$script" && "$(basename $script)" != "_common.sh" ]]; then
-    ((RUN_TESTS+=1))
-    result=$(bash "$script")
-
-    if [[ "$result" =~ success$ ]]; then
-      ((RUN_TESTS_SUCCESS+=1))
-      echo "✅ $(basename $script)"
+for T in "${!tests[@]}"; do
+  ((RUN_TESTS+=1))
+  read -r -a ARGS <<< "${tests[$T]}"
+  output=$(echo "$("$DIR/xmlmath" "${ARGS[@]}")" | xmllint --format -)
+  expect=$(cat "$DIR/test/$T.xml" | xmllint --format -)
+  
+  if [[ "$output" == "$expect" ]]; then
+    ((RUN_TESTS_SUCCESS+=1))
+    echo "✅ $T"
+  else
+    ((RUN_TESTS_ERROR+=1))
+    echo "❌ $T"
+    if [[ -n "$output" ]]; then
+      echo "$output" > "$DIR/testout_$T"
+      echo "Output written to $DIR/testout_$T"
     else
-      ((RUN_TESTS_ERROR+=1))
-      ALL_OKAY="false"
-      echo "❌ $(basename $script)"
-      while IFS= read -r line; do
-        echo "  $line"
-      done <<< "$result"
+      echo "⚠️ No output to write!"
     fi
   fi
 done
--- test/target_attr.sh
+++
@@ -1,6 +0,0 @@
-source "$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/_common.sh"
-
-echo "Target attr1"
-expect "attr add 1 -f $TEST_DEFAULT attr1" "expect-target_attr-1.xml"
-
-fin
