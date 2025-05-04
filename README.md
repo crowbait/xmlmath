d2c0865a532c78314175064ffddbcfed20490430
crowbait <31518769+crowbait@users.noreply.github.com>
2025-05-04 17:35:14 +0200

    add tests

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # xmlmath
 Simple math batch operations on XML documents
+
+curl -sL "https://domain.com/script.sh" | bash -s -- "parameter1" "parameter2"
--- test.sh
+++ test.sh
@@ -1,15 +1,33 @@
 #!/bin/bash
 
 DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-# declare common test input files
-TEST_DEFAULT="$DIR/test/test.xml"
+TESTFILE="$DIR/test/test.xml"
 
 # declare all tests
 # format: [parameters for xmlmath]
 # the expected output is test/[name of test].xml
 
 declare -A tests=(
-  [target_attr]="attr add 1 -f $TEST_DEFAULT attr1"
+  [target_attr]="attr add 1 -f $TESTFILE attr1"
+  [target_val]="value add 1.1 -f $TESTFILE val1"
+  [target_attr_multiple]="attr add 1 -f $TESTFILE attr1 attr2"
+  [target_val_multiple]="value add 1 -f $TESTFILE val1 val2"
+  [op_subtract]="value subtract 5 -f $TESTFILE val2"
+  [op_multiply]="value multiply 2 -f $TESTFILE val2"
+  [op_divide]="value divide 5 -f $TESTFILE val2"
+  [flag_int_down]="value add 5.2 --int -f $TESTFILE singleval"
+  [flag_int_up]="value add 5.6 --int -f $TESTFILE singleval"
+  [flag_min]="value subtract 9 --min 5 -f $TESTFILE singleval"
+  [flag_max]="value add 9 --max 15 -f $TESTFILE singleval"
+  [flag_regex_value]="value multiply 10 --regex -f $TESTFILE val."
+  [flag_regex_attr]="attr multiply 10 --regex -f $TESTFILE attr[0-9]"
+  [within_attr]="attr multiply 10 --within notype -f $TESTFILE attr2"
+  [within_val]="value multiply 10 --within type -f $TESTFILE val1"
+  [within_nested_parent]="value multiply 10 --within notype -f $TESTFILE val1"
+  [within_nested_child]="value multiply 10 --within med -f $TESTFILE val1"
+  [within_additional_attr]="attr multiply 10 --within notype --wa name=\"named\" -f $TESTFILE --regex attr[0-9]"
+  [within_additional_val]="value multiply 10 --within notype --wa named -f $TESTFILE val1"
+  [within_additional_nested_child]="value multiply 10 --within med --wa namedsub -f $TESTFILE val1"
 )
 
 
@@ -18,26 +36,27 @@ ALL_OKAY="true"
 RUN_TESTS=0
 RUN_TESTS_SUCCESS=0
 RUN_TESTS_ERROR=0
-rm -f "$dir/testout_*"
+rm -f $DIR/testout_*
 
 for T in "${!tests[@]}"; do
   ((RUN_TESTS+=1))
   read -r -a ARGS <<< "${tests[$T]}"
-  output=$(echo "$("$DIR/xmlmath" "${ARGS[@]}")" | xmllint --format -)
+  output=$("$DIR/xmlmath" "${ARGS[@]}")
+  output_formatted=$(echo "$output" | xmllint --format -)
   expect=$(cat "$DIR/test/$T.xml" | xmllint --format -)
   
-  if [[ "$output" == "$expect" ]]; then
+  if [[ -n "$output_formatted" && "$output_formatted" == "$expect" ]]; then
     ((RUN_TESTS_SUCCESS+=1))
     echo "✅ $T"
   else
     ((RUN_TESTS_ERROR+=1))
-    echo "❌ $T"
-    if [[ -n "$output" ]]; then
-      echo "$output" > "$DIR/testout_$T"
-      echo "Output written to $DIR/testout_$T"
-    else
-      echo "⚠️ No output to write!"
-    fi
+    echo "❌ $T: Mismatch"
+    # add verbose flag to run and dump
+    newargs="${tests[$T]/ -f/ -v -f}"
+    read -r -a ARGS <<< "$newargs"
+    output=$("$DIR/xmlmath" "${ARGS[@]}" 2>&1)
+    echo "$output" > "$DIR/testout_$T"
+    echo "Output written to $DIR/testout_$T"
   fi
 done
 
